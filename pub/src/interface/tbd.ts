11ccb5d973879f5453f4850f331b5c6c2f82e8b5
corno <corno.astn.a@schraverus.com>
2022-09-04 12:51:59 +0200

    .

--- pub/src/interface/tbd.ts
+++ pub/src/interface/tbd.ts
@@ -8,74 +8,80 @@ export type FMultiply = ($: TNumberRange) => number
 
 export type FNegative = ($: number) => number
 
-// export type FModulo = ($: {
-//     numerator: number
-//     denominator: number
-// }) => number
+/**
+ * if the denominator is 0, the return value is null
+ * (division by 0)
+ */
+export type FDivideWithRemainder = (
+    $: {
+        numerator: number
+        denominator: number
+    }
+) => null | {
+    quotient: number
+    remainder: number
+}
 
 export type FSubstract = ($: {
     minuend: number
     subtrahend: number
 }) => number
 
-export type FMax = (
-    $: {
-        /**
-         * the minimum is needed for when the array of number is empty
-         */
-        minimum: number,
-        numbers: pt.Nested<number>
-    }
+/**
+ * calculates the max of a range. If there are no entries, the result value is 0
+ */
+export type FMaxOrZero = (
+    $: pt.Nested<number>
 ) => number
 
 
-export type FDictionaryMax = (
-    $: {
-        /**
-         * the minimum is needed for when the array of number is empty
-         */
-        minimum: number,
-        numbers: pt.Dictionary<number>
-    }
-) => number
-
-export type FArrayMax = (
-    $: {
-        /**
-         * the minimum is needed for when the array of number is empty
-         */
-        minimum: number,
-        numbers: pt.Array<number>
-    }
+/**
+ * calculates the max of a dictionary of number. If there are no entries, the result value is 0
+ */
+export type FDictionaryMaxOrZero = (
+    $: pt.Dictionary<number>
 ) => number
 
 
-export type FMin = (
-    $: {
-        /**
-         * the maximum is needed for when the array of number is empty
-         */
-        maximum: number,
-        numbers: pt.Nested<number>
-    }
+/**
+ * calculates the max of an array of numbers. If there are no entries, the result value is 0
+ */
+export type FArrayMaxOrZero = (
+    $: pt.Array<number>
 ) => number
 
-export type FArrayMin = (
-    $: {
-        /**
-         * the maximum is needed for when the array of number is empty
-         */
-        maximum: number,
-        numbers: pt.Array<number>
-    }
-) => number
 
-export type FDictionaryMin = (
-    $: {
-        /**
-         * the maximum is needed for when the array of number is empty
-         */
-        maximum: number,
-        numbers: pt.Dictionary<number>
-    }
-) => number
+//MIN IS PROBLEMATIC; what is a good default value when there are no entries
+
+// export type FMin = (
+//     $: {
+//         /**
+//          * the maximum is needed for when the array of number is empty
+//          * fatal if maximum is omitted and there are no numbers
+//          */
+//         maximum: null | number,
+//         numbers: pt.Nested<number>
+//     }
+// ) => number
+
+// export type FArrayMin = (
+//     $: {
+//         /**
+//          * the maximum is needed for when the array of number is empty
+//          * fatal if maximum is omitted and there are no numbers
+//          */
+//         maximum: null | number,
+//         numbers: pt.Array<number>
+//     }
+// ) => number
+
+// export type FDictionaryMin = (
+//     $: {
+//         /**
+//          * the maximum is needed for when the array of number is empty
+//          * fatal if maximum is omitted and there are no numbers
+//          */
+//         maximum: null | number,
+//         numbers: pt.Dictionary<number>
+//     }
+// ) => number
