e5a037e4c285d51e3a06f813fb16604f8fee2eb9
corno <corno.astn.a@schraverus.com>
2022-09-04 12:32:44 +0200

    .

--- pub/src/interface/tbd.ts
+++ pub/src/interface/tbd.ts
@@ -1,19 +1,81 @@
 import * as pt from "pareto-core-types"
 
-export type NumberRange = pt.Nested<number>
+export type TNumberRange = pt.Nested<number>
 
-export type Add = ($: NumberRange) => number
+export type FAdd = ($: TNumberRange) => number
 
-export type Multiply = ($: NumberRange) => number
+export type FMultiply = ($: TNumberRange) => number
 
-export type Negative = ($: number) => number
+export type FNegative = ($: number) => number
 
-export type Modulo = ($: {
-    numerator: number
-    denominator: number
-}) => number
+// export type FModulo = ($: {
+//     numerator: number
+//     denominator: number
+// }) => number
 
-export type Substract = ($: {
+export type FSubstract = ($: {
     minuend: number
     subtrahend: number
 }) => number
+
+export type FMax = (
+    $: {
+        /**
+         * the minimum is needed for when the array of number is empty
+         */
+        minimum: number,
+        numbers: pt.Nested<number>
+    }
+) => number
+
+
+export type FDictionaryMax = (
+    $: {
+        /**
+         * the minimum is needed for when the array of number is empty
+         */
+        minimum: number,
+        numbers: pt.Dictionary<number>
+    }
+) => number
+
+export type FArrayMax = (
+    $: {
+        /**
+         * the minimum is needed for when the array of number is empty
+         */
+        minimum: number,
+        numbers: pt.Array<number>
+    }
+) => number
+
+
+export type FMin = (
+    $: {
+        /**
+         * the maximum is needed for when the array of number is empty
+         */
+        maximum: number,
+        numbers: pt.Nested<number>
+    }
+) => number
+
+export type FArrayMin = (
+    $: {
+        /**
+         * the maximum is needed for when the array of number is empty
+         */
+        maximum: number,
+        numbers: pt.Array<number>
+    }
+) => number
+
+export type FDictionaryMin = (
+    $: {
+        /**
+         * the maximum is needed for when the array of number is empty
+         */
+        maximum: number,
+        numbers: pt.Dictionary<number>
+    }
+) => number
